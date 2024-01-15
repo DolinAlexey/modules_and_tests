2053ef0c9e1d4664cf45a17d90ffe8df87cc45b9
DolinAlexey <dolin.alexey@gmail.com>
2024-01-15 20:16:23 +0300

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use modules_and_tests::pair::Pair;
 use modules_and_tests::signed::Signed;
 use modules_and_tests::unsigned::Unsigned;
-use modules_and_tests::vec3::{VEC3_LEN, Vector};
+use modules_and_tests::vec3::Vector;
 
 fn main() {
 
@@ -16,7 +16,7 @@ fn main() {
     println!("{:?}", <usize as Unsigned>::default_unsigned_counter());
     println!("{:?}", <usize as Unsigned>::next_unsigned(10));
 
-    println!("{:?}", Vector::default_vec3());
+    println!("{:?}", <[i32; modules_and_tests::vec3::Vec3] as Vector>::default_vec3());
     println!("{:?}", Vector::vec3_scalar_sum([345, 584, 33], [394, 98, 372]));
     println!("{:?}", Vector::vec3_vector_sum([1, 2, 3], [4, 5, 6]));
 }
