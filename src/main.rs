ed43e92f59f62446cd62593c0a739d2bbd99af5b
DolinAlexey <dolin.alexey@gmail.com>
2024-01-15 18:40:20 +0300

    1

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use modules_and_tests::pair::Pair;
+use modules_and_tests::signed::Signed;
+
+fn main() {
+    println!("{:?}", <(i32, i32) as Pair>::default_pair());
+    println!("{:?}", Pair::pair_scalar_sum(&(872, 962), &(772, 72)));
+    println!("{:?}", Pair::pair_vector_sum(&(345, 584), &(2, 3)));
+
+    //println!("{:?}", Signed::default_signed_counter());
+}
